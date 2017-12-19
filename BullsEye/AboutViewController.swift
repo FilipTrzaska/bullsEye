260e6756dda1a2037b71dde8f263e6d963428702
FilipTrzaska <filiptrzaska@int.pl>
2017-12-19 20:51:51 +0100

    AutoLayout changes

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+
+
+}
